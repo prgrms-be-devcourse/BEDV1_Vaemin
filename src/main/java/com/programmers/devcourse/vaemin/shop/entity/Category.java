fc5e4eed6d398fcd1554f62f2571ea40381b780e
suebeen <1003jamie@naver.com>
2021-10-30 06:10:01 +0900

    Feat: change method, builder 추가

--- src/main/java/com/programmers/devcourse/vaemin/shop/entity/Category.java
+++ src/main/java/com/programmers/devcourse/vaemin/shop/entity/Category.java
@@ -1,13 +1,33 @@
 package com.programmers.devcourse.vaemin.shop.entity;
 
 import com.programmers.devcourse.vaemin.root.IdentifiableEntity;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@Table(name = "category")
+@NoArgsConstructor
 public class Category extends IdentifiableEntity {
     @Column(name = "category_name", length = 20)
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private final List<ShopCategory> shopCategories = new ArrayList<>();
+
+    public void changeName(@NonNull String name) {
+        if(name.isBlank()) return;
+        this.name = name;
+    }
+
+    @Builder
+    public Category(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/programmers/devcourse/vaemin/shop/entity/Shop.java
+++ src/main/java/com/programmers/devcourse/vaemin/shop/entity/Shop.java
@@ -1,14 +1,24 @@
 package com.programmers.devcourse.vaemin.shop.entity;
 
+import com.programmers.devcourse.vaemin.coupon.entity.Coupon;
+import com.programmers.devcourse.vaemin.food.entity.Food;
+import com.programmers.devcourse.vaemin.order.entity.Order;
 import com.programmers.devcourse.vaemin.root.AuditableEntity;
 import com.programmers.devcourse.vaemin.user.owner.entity.Owner;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@Table(name = "shop")
+@NoArgsConstructor
 public class Shop extends AuditableEntity {
     @Column(name = "shop_name", nullable = false, length = 50)
     private String name;
@@ -58,4 +68,111 @@ public class Shop extends AuditableEntity {
 
     @Column(name = "detail_address", nullable = false)
     private String detailAddress;
+
+    @OneToMany(mappedBy = "shop")
+    private final List<DeliverySupportedRegions> regions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "shop")
+    private final List<Food> foods = new ArrayList<>();
+
+    @OneToMany(mappedBy = "shop")
+    private final List<Order> orders = new ArrayList<>();
+
+    @OneToMany(mappedBy = "shop")
+    private final List<Coupon> coupons = new ArrayList<>();
+
+    public void changeName(@NonNull String name) {
+        if (name.isBlank()) return;
+        this.name = name;
+    }
+
+    public void changePhoneNum(@NonNull String phoneNum) {
+        if (phoneNum.isBlank()) return;
+        this.phoneNum = phoneNum;
+    }
+
+    public void changeShortDescription(@NonNull String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public void changeLongDescription(@NonNull String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public void changeSupportedOrderType(@NonNull ShopSupportedOrderType supportedOrderType) {
+        this.supportedOrderType = supportedOrderType;
+    }
+
+    public void changeSupportedPayment(@NonNull ShopSupportedPayment supportedPayment) {
+        this.supportedPayment = supportedPayment;
+    }
+
+    public void changeOpenTime(@NonNull LocalDateTime openTime) {
+        this.openTime = openTime;
+    }
+
+    public void changeCloseTime(@NonNull LocalDateTime closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public void changeDeliveryFee(@NonNull int deliveryFee) {
+        if (deliveryFee < 0) return;
+        this.deliveryFee = deliveryFee;
+    }
+
+    public void changeMinOrderPrice(@NonNull int minOrderPrice) {
+        if (minOrderPrice < 0) return;
+        this.minOrderPrice = minOrderPrice;
+    }
+
+    public void changeShopStatus(@NonNull ShopStatus shopStatus) {
+        this.shopStatus = shopStatus;
+    }
+
+    public void changeRegisterNumber(@NonNull String registerNumber) {
+        if (registerNumber.isBlank()) return;
+        this.registerNumber = registerNumber;
+    }
+
+    public void changeAddress(@NonNull String doroAddress, int doroIndex, String detailAddress) {
+        this.doroAddress = doroAddress;
+        this.doroIndex = doroIndex;
+        this.detailAddress = detailAddress;
+    }
+
+    @Builder
+    public Shop(
+            String name,
+            String phoneNum,
+            String shortDesc,
+            String longDesc,
+            ShopSupportedOrderType orderType,
+            ShopSupportedPayment payment,
+            LocalDateTime openTime,
+            LocalDateTime closeTime,
+            int deliveryFee,
+            int minOrderPrice,
+            ShopStatus shopStatus,
+            String registerNumber,
+            Owner owner,
+            String doroAddress,
+            int doroIndex,
+            String detailAddress) {
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.shortDescription = shortDesc;
+        this.longDescription = longDesc;
+        this.supportedOrderType = orderType;
+        this.supportedPayment = payment;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.deliveryFee = deliveryFee;
+        this.minOrderPrice = minOrderPrice;
+        this.shopStatus = shopStatus;
+        this.registerNumber = registerNumber;
+        this.owner = owner;
+        this.doroAddress = doroAddress;
+        this.doroIndex = doroIndex;
+        this.detailAddress = detailAddress;
+    }
 }

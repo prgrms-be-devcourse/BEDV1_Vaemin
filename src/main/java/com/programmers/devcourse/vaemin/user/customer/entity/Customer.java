b17e1153bfc97d2aec2636eb1b9c00566dd532d4
이하정 <961210eee@naver.com>
2021-10-31 03:14:58 +0900

    Customer 생성자 추가

--- src/main/java/com/programmers/devcourse/vaemin/user/customer/entity/Customer.java
+++ src/main/java/com/programmers/devcourse/vaemin/user/customer/entity/Customer.java
@@ -4,10 +4,25 @@ import com.programmers.devcourse.vaemin.user.User;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 public class Customer extends User {
     @Column(name = "point", nullable = false)
     private int point; // mileage
+
+    // question : 처음 생성했을 때 updatedAt이 null일텐데 왜 nullable = false?
+    public Customer(String userName, String email, String phoneNum) {
+        this.username = userName;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.point = 0;
+    }
+
+    public Customer() {
+
+    }
 }

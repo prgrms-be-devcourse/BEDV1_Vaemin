d36cff4f3f61326dd90339b8b9bf1e8fcdb25d3b
이하정 <961210eee@naver.com>
2021-11-01 03:51:15 +0900

    파라미터 없는 success() 추가

--- src/main/java/com/programmers/devcourse/vaemin/root/ApiResponse.java
+++ src/main/java/com/programmers/devcourse/vaemin/root/ApiResponse.java
@@ -2,6 +2,7 @@ package com.programmers.devcourse.vaemin.root;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
@@ -12,6 +13,11 @@ public class ApiResponse<T> {
 
     public static <U> ApiResponse<U> success(U result) {
         return new ApiResponse<>(true, result, "");
+    }
+
+    // question : 임의로 만든건데 result값을 null로 하는 게 낫나
+    public static <U> ApiResponse<U> success() {
+        return new ApiResponse<U>(true, null, HttpStatus.NO_CONTENT.getReasonPhrase());
     }
 
     public static <U> ApiResponse<U> fail(U result, String message) {

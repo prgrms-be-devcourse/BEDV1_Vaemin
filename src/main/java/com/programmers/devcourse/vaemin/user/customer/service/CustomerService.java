823bebb2d6c311f3712137abcd9134fa24650c69
이하정 <961210eee@naver.com>
2021-11-01 02:47:56 +0900

    findOneCustomer 추가

--- src/main/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerService.java
+++ src/main/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerService.java
@@ -1,7 +1,9 @@
 package com.programmers.devcourse.vaemin.user.customer.service;
 
 import com.programmers.devcourse.vaemin.user.customer.dto.CustomerCreateRequest;
+import com.programmers.devcourse.vaemin.user.customer.dto.CustomerDetailResponse;
 import com.programmers.devcourse.vaemin.user.customer.entity.Customer;
+import com.programmers.devcourse.vaemin.user.customer.exception.CustomerExceptionSuppliers;
 import com.programmers.devcourse.vaemin.user.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,9 +19,15 @@ public class CustomerService {
     // question : 반환타입은 뭘로 해야하나? (멘토님이 update같은 경우는 객체를 반환하는 게 좋다고 하던데)
     @Transactional
     public Customer createCustomer(CustomerCreateRequest request) {
-        Customer customer = new Customer(request.getUserName(),
+        Customer newCustomer = new Customer(request.getUserName(),
                 request.getEmail(),
                 request.getPhoneNum());
-        return customerRepository.save(customer);
+        return customerRepository.save(newCustomer);
+    }
+
+    @Transactional
+    public CustomerDetailResponse findOneCustomer(Long customerId) {
+        Customer customer = customerRepository.findById(customerId).orElseThrow(CustomerExceptionSuppliers.customerNotFound);
+        return new CustomerDetailResponse(customer);
     }
 }

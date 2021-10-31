ec3b2efb5e55f2df6a326db3083d1e00d04684a9
이하정 <961210eee@naver.com>
2021-11-01 02:50:24 +0900

    getCustomer test를 위해 id getter 추가

--- src/main/java/com/programmers/devcourse/vaemin/root/IdentifiableEntity.java
+++ src/main/java/com/programmers/devcourse/vaemin/root/IdentifiableEntity.java
@@ -7,4 +7,9 @@ public class IdentifiableEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    // question : test코드 때문에 getter 만들었는데...
+    public Long getId() {
+        return id;
+    }
 }

41f08d273f8beb0beafbed245f9cadfa4d220c7b
suebeen <1003jamie@naver.com>
2021-11-01 16:19:43 +0900

    Chore: method name update

--- src/main/java/com/programmers/devcourse/vaemin/shop/controller/ShopController.java
+++ src/main/java/com/programmers/devcourse/vaemin/shop/controller/ShopController.java
@@ -18,47 +18,37 @@ public class ShopController {
     private final ShopService shopService;
     private final CategoryService categoryService;
 
-    @ExceptionHandler(NotFoundException.class)
-    public ApiResponse<String> notFoundHandler(NotFoundException e) {
-        return ApiResponse.fail(404, e.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ApiResponse<String> internalServerErrorHandler(Exception e) {
-        return ApiResponse.fail(500, e.getMessage());
-    }
-
     @PostMapping
     public ApiResponse<Long> createShop(@Valid @RequestBody ShopDto shopDto, @PathVariable Long ownerId) {
-        return ApiResponse.ok(shopService.createShop(shopDto));
+        return ApiResponse.success(shopService.createShop(shopDto));
     }
 
     @DeleteMapping("/{shopId}")
     public ApiResponse<Long> deleteShop(@Valid @PathVariable Long ownerId, @PathVariable Long shopId) {
         shopService.deleteShop(shopId);
-        return ApiResponse.ok(shopId);
+        return ApiResponse.success(shopId);
     }
 
     @GetMapping("/{shopId}")
     public ApiResponse<ShopDto> getShop(@PathVariable Long shopId, @PathVariable Long ownerId) {
         ShopDto shop = shopService.findShop(shopId);
-        return ApiResponse.ok(shop);
+        return ApiResponse.success(shop);
     }
 
     @PutMapping("/{shopId}")
     public ApiResponse<Long> updateShop(@PathVariable Long shopId, @Valid @RequestBody ShopDto shopDto, @PathVariable Long ownerId) {
-        return ApiResponse.ok(shopService.updateShop(shopId, shopDto));
+        return ApiResponse.success(shopService.updateShop(shopId, shopDto));
     }
 
     @PutMapping("/{shopId}/categories/{categoryId}")
     public ApiResponse<Long> joinShopCategory(@PathVariable Long shopId, @PathVariable Long categoryId, @PathVariable Long ownerId) {
-        return ApiResponse.ok(categoryService.joinShopCategory(shopId, categoryId));
+        return ApiResponse.success(categoryService.joinShopCategory(shopId, categoryId));
     }
 
     @DeleteMapping("/{shopId}/categories/{categoryId}")
     public ApiResponse<Long> withdrawShopCategory(@PathVariable Long shopId, @PathVariable Long categoryId, @PathVariable Long ownerId) {
         categoryService.withdrawShopCategory(shopId, categoryId);
-        return ApiResponse.ok(null);
+        return ApiResponse.success(null);
     }
 
 }

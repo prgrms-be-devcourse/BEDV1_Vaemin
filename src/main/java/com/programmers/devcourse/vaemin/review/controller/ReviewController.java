58c684bcd4aead4a98840bf41834411175737021
suebeen <1003jamie@naver.com>
2021-11-04 05:13:25 +0900

    Feat: 매장 리뷰 조회 기능 추가

--- src/main/java/com/programmers/devcourse/vaemin/review/controller/ReviewController.java
+++ src/main/java/com/programmers/devcourse/vaemin/review/controller/ReviewController.java
@@ -3,37 +3,46 @@ package com.programmers.devcourse.vaemin.review.controller;
 import com.programmers.devcourse.vaemin.review.dto.ReviewDto;
 import com.programmers.devcourse.vaemin.review.service.ReviewService;
 import com.programmers.devcourse.vaemin.root.ApiResponse;
+import com.programmers.devcourse.vaemin.shop.dto.ShopDto;
+import com.programmers.devcourse.vaemin.shop.service.ShopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping("/customers/{userId}/orders/{orderId}/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final ShopService shopService;
 
-    @PostMapping
+    @PostMapping("/customers/{userId}/orders/{orderId}/reviews")
     public ApiResponse<Long> createReview(@Valid @RequestBody ReviewDto reviewDto, @PathVariable Long userId, @PathVariable Long orderId) {
         return ApiResponse.success(reviewService.createReview(reviewDto));
     }
 
-    @GetMapping("/{reviewId}")
+    @GetMapping("/customers/{userId}/orders/{orderId}/reviews/{reviewId}")
     public ApiResponse<ReviewDto> getReview(@PathVariable Long reviewId, @PathVariable Long userId, @PathVariable Long orderId) {
         ReviewDto review = reviewService.findReview(reviewId);
         return ApiResponse.success(review);
     }
 
-    @PutMapping("/{reviewId}")
+    @PutMapping("/customers/{userId}/orders/{orderId}/reviews/{reviewId}")
     public ApiResponse<Long> updateReview(@PathVariable Long reviewId, @Valid @RequestBody ReviewDto reviewDto, @PathVariable Long userId, @PathVariable Long orderId) {
         return ApiResponse.success(reviewService.updateReview(reviewId, reviewDto));
     }
 
-    @DeleteMapping("/{reviewId}")
+    @DeleteMapping("/customers/{userId}/orders/{orderId}/reviews/{reviewId}")
     public ApiResponse<Long> deleteReview(@PathVariable Long reviewId, @PathVariable Long userId, @PathVariable Long orderId) {
         reviewService.deleteReview(reviewId);
         return ApiResponse.success(reviewId);
+    }
+
+    @GetMapping("/shops/{shopId}/reviews")
+    public ApiResponse<List<ReviewDto>> getReviews(@PathVariable Long shopId) {
+        ShopDto shop = shopService.findShop(shopId);
+        return ApiResponse.success(shop.getReviews());
     }
 }

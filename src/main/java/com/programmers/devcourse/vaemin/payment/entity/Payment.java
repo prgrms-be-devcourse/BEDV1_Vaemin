ffeaaece6eef7c270c59056d940a35f6557a321b
이하정 <961210eee@naver.com>
2021-11-04 21:06:49 +0900

    Refactor : Payment와 Order 단방향 연관관계로 수정(Payment에서 Order 삭제)

--- src/main/java/com/programmers/devcourse/vaemin/payment/entity/Payment.java
+++ src/main/java/com/programmers/devcourse/vaemin/payment/entity/Payment.java
@@ -1,6 +1,5 @@
 package com.programmers.devcourse.vaemin.payment.entity;
 
-import com.programmers.devcourse.vaemin.order.entity.Order;
 import com.programmers.devcourse.vaemin.root.AuditableEntity;
 import com.programmers.devcourse.vaemin.user.customer.entity.Customer;
 import lombok.Getter;
@@ -12,9 +11,6 @@ import javax.persistence.*;
 public class Payment extends AuditableEntity {
     @Column(name = "price", nullable = false)
     private int price;
-
-    @OneToOne(mappedBy = "payment")
-    private Order order;
 
     @ManyToOne
     @JoinColumn(name = "customer_id", referencedColumnName = "id")

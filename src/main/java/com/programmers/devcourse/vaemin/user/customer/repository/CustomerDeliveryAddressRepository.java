08de9aa0f608253af9ecf3eb63779df2e9341868
이하정 <961210eee@naver.com>
2021-11-05 21:18:52 +0900

    Feat : CustomerDeliveryAddressRepository 추가

---
+++ src/main/java/com/programmers/devcourse/vaemin/user/customer/repository/CustomerDeliveryAddressRepository.java
@@ -0,0 +1,7 @@
+package com.programmers.devcourse.vaemin.user.customer.repository;
+
+import com.programmers.devcourse.vaemin.user.customer.entity.CustomerDeliveryAddress;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerDeliveryAddressRepository extends CrudRepository<CustomerDeliveryAddress, Long> {
+}

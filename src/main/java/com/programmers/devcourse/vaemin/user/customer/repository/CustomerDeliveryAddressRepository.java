077fb8b648947e6ed1a40aac4a317f0ec22e33a7
이하정 <961210eee@naver.com>
2021-11-06 16:10:11 +0900

    Feat : CustomerDeliveryAddressRepository.findAllByCustomerId 추가

--- src/main/java/com/programmers/devcourse/vaemin/user/customer/repository/CustomerDeliveryAddressRepository.java
+++ src/main/java/com/programmers/devcourse/vaemin/user/customer/repository/CustomerDeliveryAddressRepository.java
@@ -3,5 +3,9 @@ package com.programmers.devcourse.vaemin.user.customer.repository;
 import com.programmers.devcourse.vaemin.user.customer.entity.CustomerDeliveryAddress;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerDeliveryAddressRepository extends CrudRepository<CustomerDeliveryAddress, Long> {
+    Optional<List<CustomerDeliveryAddress>> findAllByCustomerId(Long customerId);
 }

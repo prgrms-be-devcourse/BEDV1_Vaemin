170b13766737fdefc5775c17c0be12ba02fcfb0d
suebeen <1003jamie@naver.com>
2021-11-09 01:37:10 +0900

    Feat: PaymentService 수정

--- src/main/java/com/programmers/devcourse/vaemin/payment/service/PaymentService.java
+++ src/main/java/com/programmers/devcourse/vaemin/payment/service/PaymentService.java
@@ -3,6 +3,7 @@ package com.programmers.devcourse.vaemin.payment.service;
 import com.programmers.devcourse.vaemin.order.entity.OrderStatus;
 import com.programmers.devcourse.vaemin.payment.dto.PaymentDto;
 import com.programmers.devcourse.vaemin.payment.entity.Payment;
+import com.programmers.devcourse.vaemin.payment.entity.PaymentStatus;
 import com.programmers.devcourse.vaemin.payment.repository.PaymentRepository;
 import com.programmers.devcourse.vaemin.root.exception.EntityExceptionSuppliers;
 import lombok.RequiredArgsConstructor;
@@ -17,9 +18,19 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
 
     public Long createPayment(PaymentDto paymentDto) {
+
+        PaymentStatus status;
+        // payment 생성 -> not_payed, 주문 생성 완료되면 payed 변경 후 point 차감
+        if(paymentDto.getCustomer().getPoint() < paymentDto.getPrice()) {
+            status = PaymentStatus.REJECTED;
+        } else {
+            status = PaymentStatus.NOT_PAYED;
+        }
+
         Payment payment = Payment.builder()
                 .price(paymentDto.getPrice())
                 .customer(paymentDto.getCustomer())
+                .paymentStatus(status)
                 .build();
 
         return paymentRepository.save(payment).getId();
@@ -35,7 +46,7 @@ public class PaymentService {
 
         // Order 의 orderStatus -> REJECTED 변경시
         if(payment.getOrder().getOrderStatus() == OrderStatus.REJECTED) {
-            payment.changeStatus(payment.getPaymentStatus());
+            payment.changeStatus(PaymentStatus.REFUND);
         }
 
         return payment.getId();

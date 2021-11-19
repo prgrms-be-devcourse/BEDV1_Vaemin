2d58d3ac8d4a46d54bccfdbe690ca156bc1e70dc
이하정 <961210eee@naver.com>
2021-11-19 12:54:08 +0900

    Chore : delete deleteAddress, changeAddress, getAddressList

--- src/main/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerDeliveryAddressService.java
+++ src/main/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerDeliveryAddressService.java
@@ -5,71 +5,33 @@ import com.programmers.devcourse.vaemin.user.customer.dto.CustomerDeliveryAddres
 import com.programmers.devcourse.vaemin.user.customer.dto.CustomerDeliveryAddressResponse;
 import com.programmers.devcourse.vaemin.user.customer.entity.Customer;
 import com.programmers.devcourse.vaemin.user.customer.entity.CustomerDeliveryAddress;
-import com.programmers.devcourse.vaemin.user.customer.repository.CustomerDeliveryAddressRepository;
 import com.programmers.devcourse.vaemin.user.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 public class CustomerDeliveryAddressService {
-    private final CustomerDeliveryAddressRepository addressRepository;
-    // question : customerRepository를 가져오는 게 낫나 customerService를 가져오는 게 낫나?
     private final CustomerRepository customerRepository;
 
-    public CustomerDeliveryAddressService(CustomerDeliveryAddressRepository customerDeliveryAddressRepository, CustomerRepository customerRepository) {
-        this.addressRepository = customerDeliveryAddressRepository;
+    public CustomerDeliveryAddressService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     @Transactional
     public CustomerDeliveryAddressResponse getAddress(Long customerId) {
         Customer customer = customerRepository.findById(customerId).orElseThrow(EntityExceptionSuppliers.customerNotFound);
-        return new CustomerDeliveryAddressResponse(customer.getLocationCode(), customer.getAddressDetail());
+        return new CustomerDeliveryAddressResponse(customer.getCurrentAddress().getLocationCode(), customer.getCurrentAddress().getLocationCode());
     }
 
-    @Transactional
-    public List<CustomerDeliveryAddressResponse> getAddressList(Long customerId) {
-        return addressRepository.findAllByCustomerId(customerId)
-                .stream()
-                .map(CustomerDeliveryAddressResponse::new)
-                .collect(Collectors.toList());
-    }
+    // TODO: 2021-11-19 getAllAddress , addAddress, deleteAddress, changeAddress
 
     @Transactional
-    public CustomerDeliveryAddressResponse addAndUpdateAddress(Long customerId, CustomerDeliveryAddressRequest request) {
+    public CustomerDeliveryAddressResponse updateAddress(Long customerId, CustomerDeliveryAddressRequest request) {
         Customer customer = customerRepository.findById(customerId).orElseThrow(EntityExceptionSuppliers.customerNotFound);
-        customer.changeLocationCode(request.getLocationCode());
-        customer.changeAddressDetail(request.getAddressDetail());
-        Customer updatedCustomer = customerRepository.save(customer);   // 주소 수정한 customer
-        CustomerDeliveryAddress address = addressRepository.save(new CustomerDeliveryAddress(request.getLocationCode(), request.getAddressDetail(), updatedCustomer));
-        return new CustomerDeliveryAddressResponse(address);
+        CustomerDeliveryAddress newAddress = new CustomerDeliveryAddress(request.getLocationCode(), request.getAddressDetail());
+        customer.changeAddress(newAddress);
+        customerRepository.save(customer);   // 주소 수정한 customer
+        return new CustomerDeliveryAddressResponse(newAddress);
     }
 
-    @Transactional
-    public void deleteAddress(Long customerId, Long addressId) {
-        CustomerDeliveryAddress address = addressRepository.findById(addressId).orElseThrow(EntityExceptionSuppliers.customerAddressNotFound);
-        if (!customerId.equals(address.getCustomer().getId())) {
-            throw new IllegalArgumentException("This customer has no access to address.");
-        }
-        Customer customer = customerRepository.findById(customerId).orElseThrow(EntityExceptionSuppliers.customerNotFound);
-        if (customer.getLocationCode().equals(address.getLocationCode()) &&
-                customer.getAddressDetail().equals(address.getAddressDetail())) {
-            // 현재 customer에 설정된 주소면 exception
-            throw new IllegalArgumentException("This address is customer's current address");
-        }
-        addressRepository.deleteById(addressId);
-    }
-
-    @Transactional
-    public CustomerDeliveryAddressResponse changeAddress(Long customerId, Long addressId) {
-        CustomerDeliveryAddress address = addressRepository.findById(addressId).orElseThrow(EntityExceptionSuppliers.customerAddressNotFound);
-        Customer customer = customerRepository.findById(customerId).orElseThrow(EntityExceptionSuppliers.customerAddressNotFound);
-        customer.changeLocationCode(address.getLocationCode());
-        customer.changeAddressDetail(address.getAddressDetail());
-        customerRepository.save(customer);
-        return new CustomerDeliveryAddressResponse(address);
-    }
 }

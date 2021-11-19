14d80b7edb6b831266f8aac1520b2514beaca330
Kwonkyu <park2348190@naver.com>
2021-11-20 00:38:54 +0900

    Updated other tests. - jpa needs transaction.

--- src/test/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerDeliveryAddressServiceTest.java
+++ src/test/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerDeliveryAddressServiceTest.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class CustomerDeliveryAddressServiceTest {
     @Autowired
     private CustomerDeliveryAddressService addressService;
--- src/test/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerServiceTest.java
+++ src/test/java/com/programmers/devcourse/vaemin/user/customer/service/CustomerServiceTest.java
@@ -10,11 +10,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
+@Transactional
+@ActiveProfiles("test")
 class CustomerServiceTest {
     @Autowired
     private CustomerService customerService;

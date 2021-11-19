2389fe094c187e39a38c48a6cba502836f5b8694
이하정 <961210eee@naver.com>
2021-11-19 12:50:15 +0900

    Chore : Change CustomerDeliveryAddress into Embedded Type

--- src/main/java/com/programmers/devcourse/vaemin/user/customer/entity/Customer.java
+++ src/main/java/com/programmers/devcourse/vaemin/user/customer/entity/Customer.java
@@ -27,23 +27,12 @@ public class Customer extends User {
     @OneToMany(mappedBy = "customer", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private final List<Order> orders = new ArrayList<>();
 
+    @Embedded
+    private CustomerDeliveryAddress currentAddress;
+
     public void addOrder(Order order) {
         this.orders.add(order);
         order.changeCustomer(this);
-    }
-  
-    @Column(name = "location_code", nullable = false, length = 20)
-    private String locationCode;
-
-    @Column(name = "address_detail", nullable = false, length = 50)
-    private String addressDetail;
-
-    public void changeLocationCode(String locationCode) {
-        this.locationCode = locationCode;
-    }
-
-    public void changeAddressDetail(String addressDetail) {
-        this.addressDetail = addressDetail;
     }
 
     public void addCoupon(Coupon coupon) {
@@ -54,13 +43,15 @@ public class Customer extends User {
         coupon.getCustomers().add(customerCoupon);
     }
 
+    public void changeAddress(CustomerDeliveryAddress address) {
+        this.currentAddress = address;
+    }
     @Builder
-    public Customer(String userName, String email, String phoneNum, String locationCode, String addressDetail) {
+    public Customer(String userName, String email, String phoneNum, CustomerDeliveryAddress currentAddress) {
         this.username = userName;
         this.email = email;
         this.phoneNum = phoneNum;
         this.point = 0;
-        this.locationCode = locationCode;
-        this.addressDetail = addressDetail;
+        this.currentAddress = currentAddress;
     }
 }
